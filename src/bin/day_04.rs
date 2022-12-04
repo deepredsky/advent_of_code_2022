19f23407a10c685d99576ed23fed144736421947
Rajesh Sharma <rajesh.sharma@zendesk.com>
2022-12-04 16:37:00 +0100

    day 4 part 2

--- src/bin/day_04.rs
+++ src/bin/day_04.rs
@@ -16,34 +16,29 @@ fn parse_input(path: &str) -> Vec<SectionPair> {
         .expect("should read file")
         .lines()
         .map(|x| x.to_string())
-        .map(|x| {
-             x.split(",")
-             .map(|y| y.to_string()).collect::<Vec<String>>()
-            }
-        )
+        .map(|x| x.split(",").map(|y| y.to_string()).collect::<Vec<String>>())
         .map(|x| // ["2kk3", "4..5"]
-             x.iter().map(|r| convert_to_range(r.to_string())).collect::<SectionPair>()
-             )
+             x.iter().map(|r| convert_to_range(r.to_string())).collect::<SectionPair>())
         .collect()
 }
 
 // 1-5
 fn convert_to_range(input: String) -> Range<usize> {
     let parsed = input
-        .split("-").map(|x| x.to_string().parse::<usize>().unwrap())
+        .split("-")
+        .map(|x| x.to_string().parse::<usize>().unwrap())
         .collect::<Vec<usize>>();
     let a = parsed[0];
     let b = parsed[1];
 
     if parsed[0] == parsed[1] {
-        a..(a+1)
+        a..(a + 1)
     } else {
-        a..(b+1)
+        a..(b + 1)
     }
 }
 
 // "1-5" -> 1..5
-
 
 fn wat(section_pair: SectionPair) -> bool {
     let a = &section_pair[0];
@@ -51,15 +46,20 @@ fn wat(section_pair: SectionPair) -> bool {
     let b = &section_pair[1];
     let bc = b.clone().collect::<Vec<usize>>();
 
+    let x = ac.intersect(bc);
+
+    x.len() == a.len() || x.len() == b.len()
+}
+
+fn wat1(section_pair: SectionPair) -> bool {
+    let a = &section_pair[0];
+    let ac = a.clone().collect::<Vec<usize>>();
+    let b = &section_pair[1];
+    let bc = b.clone().collect::<Vec<usize>>();
 
     let x = ac.intersect(bc);
 
-    assert_eq!(vec![1].intersect(vec![1,2]), vec![1]);
-    assert_eq!(vec![1,2].intersect(vec![1]), vec![1]);
-
-    println!("x {:?}", x);
-
-    x.len() == a.len() || x.len() == b.len()
+    x.len() > 0
 }
 
 fn part_1(input: Input) -> usize {
@@ -69,11 +69,17 @@ fn part_1(input: Input) -> usize {
             count += 1
         }
     }
-            count
+    count
 }
 
-fn part_2(rucksacks: Vec<String>) -> u32 {
-    0
+fn part_2(input: Input) -> usize {
+    let mut count = 0;
+    for section_pair in input {
+        if wat1(section_pair) {
+            count += 1
+        }
+    }
+    count
 }
 
 fn solve_part_1(input: Input) {
@@ -83,7 +89,7 @@ fn solve_part_1(input: Input) {
     println!("Part 1 time: {:.2?} \n", part1_start.elapsed());
 }
 
-fn solve_part_2(input: Vec<String>) {
+fn solve_part_2(input: Input) {
     let part2_start = Instant::now();
     let part_2_result = part_2(input);
     println!("Part 2 answer: {}", part_2_result);
@@ -95,5 +101,5 @@ fn main() {
     let input = parse_input("data/day-04.txt");
     // println!("input: {:?}", input);
     solve_part_1(input.clone());
-//    solve_part_2(input.clone());
+    solve_part_2(input.clone());
 }
